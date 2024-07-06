5bf78677a396ff6f8cee95a6ba8f0abe7108d834
minuchavan <minalchavan260@gmail.com>
2024-07-06 10:20:46 +0530

    cpp day

---
+++ Program1Const.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number=6;
+    const int i=10;
+    cout<< "\n i :"<<i;
+    constexpr int num =34 *3;
+    cout<<"\n num :"<<num;
+    return 0;
+}
+
+
+//output i=10  num=102
